d79aaf3d233f4171d2e4c011353682037042c1d3
Jesse Packwood <packwoodjustin@gmail.com>
2017-10-10 08:49:38 -0600

    update tests and add score increase function

--- test/ball-test.js
+++ test/ball-test.js
@@ -1,7 +1,9 @@
 const { assert } = require('chai');
+const { expect } = require('chai');
 const Ball = require('../lib/Ball.js');
 const Paddle = require('../lib/Paddle.js');
 const Brick = require('../lib/Brick.js');
+const Game = require('../lib/Game.js')
 
 let ball;
 
@@ -19,4 +21,31 @@ it('should have a x coordinate', () => {
 
 it('should have a y coordinate', () => {
   assert.equal(ball.y, 100)
+})
+
+it('should bounce off the top of the canvas', () => {
+  // console.log(ball.bounceWalls())
+  assert.equal(ball.Yvelocity, -2, true)
+  ball.bounceWalls();
+  assert.equal(ball.Yvelocity, 2, true)
+})
+
+it('should bounce off the right wall', () => {
+  assert.equal(ball.Xvelocity, 2, true)
+  ball.bounceWalls();
+  assert.equal(ball.Xvelocity, -2, true)
+})
+
+it('should bounce off the left wall', () => {
+  assert.equal(ball.Xvelocity, -2, true)
+  ball.bounceWalls();
+  assert.equal(ball.Xvelocity, 2, true)
+})
+
+it.skip('should bounce off the paddle', () => {
+  
+})
+
+it.skip('should bounce off a brick', () => {
+  
 })
--- test/paddle-test.js
+++ test/paddle-test.js
@@ -1,5 +1,6 @@
 const { assert } = require('chai');
 const Paddle = require('../lib/Paddle.js');
+const Game = require('../lib/Game.js')
 
 describe('Paddle testing', () => {
   let paddle;
@@ -8,11 +9,17 @@ describe('Paddle testing', () => {
     paddle = new Paddle(300, 100, 100, 15);
   })
 
-  it('should be an instance of Paddle', () => {
-    assert.equal(paddle instanceof (Paddle), true);
+  it('should instantiate a new Paddle', () => {
+    assert.isObject(paddle);
   })
 
   it('should have x coordinate', () => {
     assert.equal(paddle.x, 300);
   })
+
+  it('should have a y coordinate', () => {
+    assert.equal(paddle.y, 100)
+  })
+
+  
 })
